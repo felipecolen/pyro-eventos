98bed646ab5e4f2f7ed5cc4190cd23f431755732
Felipe Oliveira Colen <felipecolen@gmail.com>
2019-04-28 15:27:44 -0400

    correcao no nome

--- app_evento/models/apoiador.py
+++ app_evento/models/apoiador.py
@@ -3,10 +3,10 @@ from django.db import models
 from app_evento.functions import gerar_codigo_alfanumerico, obter_extensao_arquivo
 
 
-class Apoiadxr(models.Model):
+class Apoiador(models.Model):
 
     def local_logo(self, filename):
-        return '{}/{}_{}{}'.format('apoiadorxs', self.id, gerar_codigo_alfanumerico(6), obter_extensao_arquivo(filename))
+        return '{}/{}_{}{}'.format('apoiadores', self.id, gerar_codigo_alfanumerico(6), obter_extensao_arquivo(filename))
 
     nome = models.CharField('Nome', max_length=100)
     link = models.CharField('Link', max_length=200, null=True, blank=True, default='http://',
@@ -23,5 +23,5 @@ class Apoiadxr(models.Model):
 
     class Meta:
         ordering = ['nome']
-        verbose_name = 'Apoiadxr'
+        verbose_name = 'Apoiador(a)'
         verbose_name_plural = 'Apoiadorxs'
